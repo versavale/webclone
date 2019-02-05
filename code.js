61970710e90ad88e2e715d08eccc3c16cfc7be94
Valentina Versari <versavale@gmail.com>
2019-02-05 07:54:13 +0100

    fixed typewriter

--- code.js
+++ code.js
@@ -50,7 +50,12 @@ window.onload = function() {
       new TxtType(elements[i], JSON.parse(toRotate), period);
     }
   }
-}
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
 
 
 window.onscroll = function() {
